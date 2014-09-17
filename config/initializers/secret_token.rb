9810d3c975f6b78143d0046c772e0bd76e5e750d
Todd <laugh_aloud80026@yahoo.com>
2014-09-17 20:03:42 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scabber::Application.config.secret_key_base = '2ccc1f6457fa77ffd9088ff4b9e84effca9e8db3ddf6f486fb217fc2354929ef2f627baed2747b65a87fd32b1a02d985a24aea14855be1f51ba9846d17f4e719'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scabber::Application.config.session_store :cookie_store, key: '_scabber_session'
