64bd4446aeb9a9c2396ea23417f58d6c68eee95e
Todd <laugh_aloud80026@yahoo.com>
2014-10-01 02:03:15 +0000

    Added comment and Rating boxes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Scabber::Application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root 'places#index'
+  root 'places#index'
 
-resources :places
+  resources :places do
+    resources :comments, :only => :create
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
